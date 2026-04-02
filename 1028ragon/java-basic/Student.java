79f8b077fc1a8bcf5040fd7151d378dbd2ab0fbd
1028ragon <1028ragon@naver.com>
2026-04-02 09:49:48 +0900

    Feat/ [객체 배열 관리 프로그램] 유소영 과제 제출

---
+++ 1028ragon/java-basic/Student.java
@@ -0,0 +1,12 @@
+public class Student {
+    String name;
+    int studentId;
+    String major;
+
+    Student(String name, int studentId, String major) {
+        this.name = name;
+        this.studentId = studentId;
+        this.major = major;
+    }
+}
+
