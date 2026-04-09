9b945cd8e55e67ae9c92343552fa89ea89fadce5
이민구 <minet0401@gmail.com>
2026-04-09 10:35:14 +0900

    Feat/Department 클래스 생성

---
+++ java-opp1/model/Department.java
@@ -0,0 +1,22 @@
+package model;
+
+public class Department {
+    private String name;
+    private  int studentCount;
+    private String building;
+
+
+    public Department(String name,int studentCount,String building){
+        this.name = name;
+        this.studentCount = studentCount;
+        this.building = building;
+    }
+
+    public void printAll(Department department){
+        System.out.printf("학과명: %s\n학생 수: %d\n건물: %s\n",department.name,department.studentCount,department.building);
+    }
+
+
+
+
+}
