8efbe379d3a563ce38e506d6b583039efdc5f225
이민구 <minet0401@gmail.com>
2026-04-17 14:07:07 +0900

    Fix/음수 출력 요청 +출금 후 잔액이 0원일때도 가능

--- java-oop2/model/bankservice/Withdrawal.java
+++ java-oop2/model/bankservice/Withdrawal.java
@@ -4,8 +4,12 @@ import model.Account;
 public class Withdrawal implements Transactions{
     @Override
     public boolean transaction(Account account, int money){
+        if(money<=0){
+            return false;
+        }
+
         int minusAccount = account.getBalance()-money;
-        if(minusAccount<=account.MIN_BALABCE){
+        if(minusAccount<account.MIN_BALABCE){ㅌ
             return false;
         }else{
             account.setBalance(minusAccount);
