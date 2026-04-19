09f355ce7efcae9313a9ecb5f4281c7a6326794e
이민구 <minet0401@gmail.com>
2026-04-19 16:27:02 +0900

    Refactor/ UserInformation으로 정보 출력 통합

--- java-oop2/view/message/accountinformation/UserInformation.java
+++ java-oop2/view/message/accountinformation/UserInformation.java
@@ -6,4 +6,7 @@ public class UserInformation {
     public static void userinformation(Account account){
         System.out.printf("*계좌번호:%d 이름:%s*\n",account.getAccountNumber(),account.getOwner());
     }
+    public static void printMoney(Account account){
+        System.out.printf("*현재 금액은 %d원입니다*\n",account.getBalance());
+    }
 }
