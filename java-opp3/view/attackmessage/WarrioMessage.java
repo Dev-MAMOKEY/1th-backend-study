d0e4ef3ca473c805a12997163b0a2e44c557bcbc
이민구 <minet0401@gmail.com>
2026-04-23 20:56:04 +0900

    Feat/ Warrio attack 메세지

---
+++ java-opp3/view/attackmessage/WarrioMessage.java
@@ -0,0 +1,8 @@
+package view.attackmessage;
+
+public class WarrioMessage extends AttackMessage{
+    @Override
+    public void attackMessage(){
+        System.out.println("Warrior attacks with sword!");
+    }
+}
