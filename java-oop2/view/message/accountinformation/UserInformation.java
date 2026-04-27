e8416539024e2173080ffcbf13372e4b6bd2e993
이민구 <minet0401@gmail.com>
2026-04-27 18:16:52 +0900

    Fix/ 띄어쓰기 수정

--- java-oop2/view/message/accountinformation/UserInformation.java
+++ java-oop2/view/message/accountinformation/UserInformation.java
@@ -2,7 +2,7 @@ package view.message.accountinformation;
 
 import model.Account;
 
-public class UserInformation {
+public class UserInformation{
     public static void userinformation(Account account){
         System.out.printf("*계좌번호:%d 이름:%s*\n",account.getAccountNumber(),account.getOwner());
     }
