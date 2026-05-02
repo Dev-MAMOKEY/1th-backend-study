236c8519897dd0b277f79ac46864ec250a82367f
이민구 <minet0401@gmail.com>
2026-05-03 00:57:01 +0900

    Fix/ 장치별 최대 최소 volume 메세지 출력 수정

--- java-oop4/view/ErrorMessage.java
+++ java-oop4/view/ErrorMessage.java
@@ -1,11 +1,11 @@
 package view;
 
 public class ErrorMessage {
-    public static void volumeupError(){
-        System.out.println("100이 최대 볼륨 입니다");
+    public static void volumeupError(String name, int maxVolume){
+        System.out.printf("%s의 최대 volume은 %d입니다",name,maxVolume);
     }
-    public static void volumedownError(){
-        System.out.println("0이하로 볼륨을 낮출 수 없습니다 ");
+    public static void volumedownError(String name, int minVolume){
+        System.out.printf("%s의 최소 volume은 %d입니다",name,minVolume);
     }
     public static void powerCondition(){
         System.out.println("*전원이 켜져있을때만 volum 조절 가능합니다*");
