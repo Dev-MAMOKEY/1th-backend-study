28754b9ea8f53c946942daf87768e67c62f3cad0
이민구 <minet0401@gmail.com>
2026-05-04 14:36:00 +0900

    Fix/camelCase에 맞게 userInformation() 수정

--- java-oop2/view/message/accountinformation/UserInformation.java
+++ java-oop2/view/message/accountinformation/UserInformation.java
@@ -3,7 +3,7 @@ package view.message.accountinformation;
 import model.Account;
 
 public class UserInformation{
-    public static void userinformation(Account account){
+    public static void userInformation(Account account){
         System.out.printf("*계좌번호:%d 이름:%s*\n",account.getAccountNumber(),account.getOwner());
     }
     public static void printMoney(Account account){
