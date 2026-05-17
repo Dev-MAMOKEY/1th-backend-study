a9f8c8f9dca6aa14cc642e8170b70d2283576a40
이민구 <minet0401@gmail.com>
2026-05-17 17:42:53 +0900

    Refacotr/ 출력이 아닌 AttackMessage 형식으로 전달

--- java-opp3/controller/AttackController.java
+++ java-opp3/controller/AttackController.java
@@ -5,16 +5,15 @@ import view.attackmessage.MageMessage;
 import view.attackmessage.WarrioMessage;
 
 public class AttackController {
-    private AttackMessage attackMessage;
 
-    public void choiceAttack(int choice){
-        if(choice ==1){
-            attackMessage = new MageMessage();
-        }else if (choice == 2){
-            attackMessage = new WarrioMessage();
-        }else{
-            attackMessage  = new AttackMessage();
+
+    public AttackMessage choiceAttack(int choice) {
+        if (choice == 1) {
+            return new MageMessage();
+        } else if (choice == 2) {
+            return new WarrioMessage();
+        } else {
+            return new AttackMessage();
         }
-        attackMessage.attackMessage();
     }
 }
