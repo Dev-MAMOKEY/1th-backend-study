31751e99b9fedd9d8f5fd30a7641f552638c1665
이민구 <minet0401@gmail.com>
2026-05-20 12:27:13 +0900

    Refactor/Mvc 구조로 수정, Runner 호출

--- java-basic/Main.java
+++ java-basic/Main.java
@@ -1,17 +1,5 @@
-import model.Student;
-
 public class Main {
     public static void main(String[] args) {
-        Student s1 = new Student("김멋사","20260001","Software");
-        Student s2 = new Student("김동사","20250001","Hardware");
-        Student s3 = new Student("김수사","20240001","AI");
-
-        Student[] students = {s1,s2,s3};
-
-        Student.printAll(students);
-
-
-
-
+        Runner.run();
     }
 }
