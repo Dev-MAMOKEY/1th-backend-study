b4ce661cb909477e031dda420967c5d0d64efcd3
이민구 <minet0401@gmail.com>
2026-05-27 00:28:00 +0900

    Refactory/ dto title,content 글자수 제한

--- springmvc/src/main/java/post/dto/PostUpdateRequest.java
+++ springmvc/src/main/java/post/dto/PostUpdateRequest.java
@@ -1,9 +1,12 @@
 package post.dto;
 
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 @Getter
 public class PostUpdateRequest {
+    @Size(max = 100, message = "100자만 가능합니다.")
     private String title;
+    @Size(max =500, message = "200자만 가능합니다")
     private String content;
 }
