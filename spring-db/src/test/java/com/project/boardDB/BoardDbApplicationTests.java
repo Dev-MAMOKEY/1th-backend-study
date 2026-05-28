0bf4a6540d92745659ea356a8dfc4d550ca3358b
1028ragon <1028ragon@naver.com>
2026-05-28 21:32:14 +0900

    Rename/ 폴더명 spring-db로 수정

---
+++ spring-db/src/test/java/com/project/boardDB/BoardDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.boardDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
