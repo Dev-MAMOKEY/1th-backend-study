bc790cbb93d5d3c47795ee8304f7f656dc7a8e84
1028ragon <1028ragon@naver.com>
2026-05-28 21:17:14 +0900

    init/ 프로젝트 초기생성 + .env 파일로 민감한 정보 분리

---
+++ boardDB/src/main/java/com/project/boardDB/BoardDbApplication.java
@@ -0,0 +1,13 @@
+package com.project.boardDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardDbApplication.class, args);
+	}
+
+}
