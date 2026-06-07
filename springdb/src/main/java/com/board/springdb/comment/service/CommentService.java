ae82fa67955b6e99d586b99a72634ec2ca63378c
이민구 <minet0401@gmail.com>
2026-06-08 00:34:43 +0900

    Refactor/Transactional 수정

--- springdb/src/main/java/com/board/springdb/comment/service/CommentService.java
+++ springdb/src/main/java/com/board/springdb/comment/service/CommentService.java
@@ -83,6 +83,7 @@ public class CommentService {
     }
 
     // 댓글 삭제
+    @Transactional
     public void deleteComment(Long commentId){
         Comment comment = commentRepository.findById(commentId).orElseThrow(IllegalArgumentException::new);
         commentRepository.delete(comment);
