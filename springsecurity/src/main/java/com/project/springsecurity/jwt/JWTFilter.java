d6341c4f9c5b4fb19dbcd2c640ac3edb8530b92d
이민구 <minet0401@gmail.com>
2026-06-12 19:09:07 +0900

    Feat/ JWT 토큰을 검증

---
+++ springsecurity/src/main/java/com/project/springsecurity/jwt/JWTFilter.java
@@ -0,0 +1,61 @@
+package com.project.springsecurity.jwt;
+
+import com.project.springsecurity.config.JWTUtil;
+import com.project.springsecurity.dto.CustomUserDetails;
+import com.project.springsecurity.entity.UserEntity;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+public class JWTFilter extends OncePerRequestFilter {
+
+    private final JWTUtil jwtUtil;
+
+    public JWTFilter(JWTUtil jwtUtil) {
+        this.jwtUtil = jwtUtil;
+    }
+
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+
+        String authorization= request.getHeader("Authorization");
+
+
+        if (authorization == null || !authorization.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+
+            return;
+        }
+
+        String token = authorization.split(" ")[1];
+
+        if (jwtUtil.isExpired(token)) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        String username = jwtUtil.getUsername(token);
+        String role = jwtUtil.getRole(token);
+
+        UserEntity userEntity = UserEntity.builder().username(username).password("temppassword").role(role)
+                .build();
+
+        CustomUserDetails customUserDetails = new CustomUserDetails(userEntity);
+
+        //스프링 시큐리티 인증 토큰 생성
+        Authentication authToken = new UsernamePasswordAuthenticationToken(customUserDetails, null, customUserDetails.getAuthorities());
+        //세션에 사용자 등록
+        SecurityContextHolder.getContext().setAuthentication(authToken);
+
+        filterChain.doFilter(request, response);
+    }
+}
