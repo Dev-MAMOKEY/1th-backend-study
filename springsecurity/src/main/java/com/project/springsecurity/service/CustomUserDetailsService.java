ba68a3713551db46b9001682f6b74da92b33d034
이민구 <minet0401@gmail.com>
2026-06-12 19:06:11 +0900

    Feat/ CustomUserDetailsService(UserDetailsService상속)

---
+++ springsecurity/src/main/java/com/project/springsecurity/service/CustomUserDetailsService.java
@@ -0,0 +1,23 @@
+package com.project.springsecurity.service;
+
+import com.project.springsecurity.dto.CustomUserDetails;
+import com.project.springsecurity.entity.UserEntity;
+import com.project.springsecurity.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+        private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username)
+                .map(CustomUserDetails::new)
+                .orElseThrow(() -> new UsernameNotFoundException("유저를 찾을 수 없습니다."));
+        }
+}
