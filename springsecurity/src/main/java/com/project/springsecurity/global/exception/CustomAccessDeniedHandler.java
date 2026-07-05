553ecccd0305ff05baf5ea3296f8406390b15ea2
이민구 <minet0401@gmail.com>
2026-07-05 18:07:18 +0900

    Feat/ 401,403 오류 ㅜ가

---
+++ springsecurity/src/main/java/com/project/springsecurity/global/exception/CustomAccessDeniedHandler.java
@@ -0,0 +1,39 @@
+package com.project.springsecurity.global.exception;
+
+import com.project.springsecurity.global.RsData.RsData;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+import tools.jackson.databind.ObjectMapper;
+
+
+import java.io.IOException;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+
+    private final ObjectMapper objectMapper;
+
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
+        log.error("No Authorities", accessDeniedException);
+        log.error("Reqeust Uri : {}", request.getRequestURI());
+
+        String responseContent = objectMapper.writeValueAsString(new RsData<>("403-1", "접근 권한이 없습니다."));
+
+        response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setCharacterEncoding("UTF-8");
+        response.getWriter().write(responseContent);
+
+
+    }
+}
